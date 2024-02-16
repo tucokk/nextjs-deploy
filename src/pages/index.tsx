0884b77a2994da0c69124bcc37790cadbb5b91ca
Arthur Ribeiro <arthurluftribeiro@gmail.com>
2024-02-16 13:40:05 -0300

    nova home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,26 +5,7 @@ import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Contador/>
-    </div>
-  );
-}
-
-function Contador() {
-
-  const [contador, setContador] = useState<number>(0);
-  const adicionarContador = () => { 
-    setContador(contador + 1);
-  }
-  return (
-    <div>
-      <div>
-        <div>{contador}</div>
-        <button onClick={adicionarContador}>Adicionar</button>
-      </div>
-    </div>
-  )
+    return (
+        <h1>Home</h1>
+    )
 }
