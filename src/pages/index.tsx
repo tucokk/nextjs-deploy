5838ac30b725c964ebe3be94f99556cb56ae3f1a
Arthur Ribeiro <arthurluftribeiro@gmail.com>
2024-02-16 13:45:36 -0300

    nova página inicial teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useState } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <h1>Página Inicial</h1>
+        <div>
+          <h1>Página Inicial</h1>
+        </div>
     )
 }
